a84e396d43934c57dcc2a5a7a1e78f32f2dc0133
72-S <consti.bauer@outlook.com>
2025-02-10 12:38:20 +0100

    changed deprecated parameters

--- home/.config/nvim/lua/plugins/configs/markdown.lua
+++ home/.config/nvim/lua/plugins/configs/markdown.lua
@@ -6,34 +6,32 @@ return {
         "nvim-tree/nvim-web-devicons"
     },
     config = function()
-        local presets = require("markview.presets");
+        local presets = require("markview.presets")
         require("markview").setup({
             checkboxes = presets.checkboxes.nerd,
-            headings = presets.headings.glow_center,
-            horizontal_rules = presets.horizontal_rules.dotted,
+            markdown = {
+                headings = presets.headings.glow_center,
+                horizontal_rules = presets.horizontal_rules.dotted,
+            },
             preview = {
                 enable = false,
-                filetypes = { "md", "rmd", "quarto"},
+                filetypes = { "md", "rmd", "quarto" },
             },
             code_blocks = {
                 style = "language",
-
                 language_direction = "right",
                 min_width = 60,
                 pad_char = " ",
                 pad_amount = 3,
-
+                border_hl = "MarkviewCode",
+                info_hl = "MarkviewCodeInfo",
+                sign = true,
+                sign_hl = nil,
                 language_names = {
                     ["txt"] = "Text"
                 },
-
-                hl = "MarkviewCode",
-                info_hl = "MarkviewCodeInfo",
-
-                sign = true,
-                sign_hl = nil
             },
         })
     end,
-
 }
+
