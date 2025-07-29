a1468e7a32773d4b7f164b27a330065f2965fa03
objz <me@objz.dev>
2025-07-29 23:41:38 +0200

    fixed keybinds issue

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -1,60 +1,71 @@
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
 })
 
 vim.filetype.add({
-    extension = {
-        zsh = "sh",
-        sh = "sh",
-    },
-    filename = {
-        [".zshrc"] = "sh",
-        ["zshrc"] = "sh",
-        [".zshenv"] = "sh",
-    },
+	extension = {
+		zsh = "sh",
+		sh = "sh",
+	},
+	filename = {
+		[".zshrc"] = "sh",
+		["zshrc"] = "sh",
+		[".zshenv"] = "sh",
+	},
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client.name ~= "jdtls" then
+			return
+		end
+
+		local bufnr = args.buf
+		require("which-key").add({
+			{ "<leader>l",  group = "LSP Actions" },
+			{ "<leader>lo", "<cmd>lua require'jdtls'.organize_imports()<cr>",     desc = "Organize Imports" },
+			{ "<leader>ls", "<cmd>lua require'jdtls'.super_implementation()<cr>", desc = "Super Implementation" },
+			{ "<leader>lr", "<cmd>LspRestart<cr>",                                desc = "Restart Server" },
+			{ "<leader>lu", "<cmd>JdtUpdateConfig<cr>",                           desc = "Update Config" },
+			{ "<leader>r",  group = "Run Configurations" },
+			{ "<leader>rd", "<cmd>9Multiterm ./gradlew dev<cr>",                  desc = "gradlew dev" },
+			{ "<leader>rb", "<cmd>9Multiterm ./gradlew build<cr>",                desc = "gradlew build" },
+			{ "<leader>rs", "<cmd>9Multiterm ./gradlew shadowJar<cr>",            desc = "gradlew shadowJar" },
+			{ "<leader>rm", "<cmd>9Multiterm ./gradlew modrinth<cr>",             desc = "gradlew modrinth" },
+		}, { buffer = bufnr })
+	end,
 })
 
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        local bufnr = args.buf
-        if client.name ~= "jdtls" then
-            require("which-key").add({
-                { "<leader>l",  group = "LSP Actions" },
-                { "<leader>lo", "<cmd>lua require'jdtls'.organize_imports()<cr>",     desc = "Organize Imports" },
-                { "<leader>ls", "<cmd>lua require'jdtls'.super_implementation()<cr>", desc = "Super Implementation" },
-                { "<leader>lr", "<cmd>LspRestart<cr>",                                desc = "Restart Server" },
-                { "<leader>lu", "<cmd>JdtUpdateConfig<cr>",                           desc = "Update Config" },
-                { "<leader>r",  group = "Run Configurations" },
-                { "<leader>rd", "<cmd>9Multiterm ./gradlew dev<cr>",                  desc = "gradlew dev" },
-                { "<leader>rb", "<cmd>9Multiterm ./gradlew build<cr>",                desc = "gradlew build" },
-                { "<leader>rs", "<cmd>9Multiterm ./gradlew shadowJar<cr>",            desc = "gradlew shadowJar" },
-                { "<leader>rm", "<cmd>9Multiterm ./gradlew modrinth<cr>",             desc = "gradlew modrinth" },
-            }, { buffer = bufnr })
-        elseif client.name == "rust_analyzer" then
-            require("which-key").add({
-                    { "<leader>l",  group = "LSP Actions" },
-                    { "<leader>le", "<cmd>RustExpand<cr>",              desc = "Expand" },
-                    { "<leader>lr", "<cmd>LspRestart<cr>",              desc = "Restart Server" },
-                    { "<leader>r",  group = "Run Configurations" },
-                    { "<leader>rd", "<cmd>9Multiterm cargo build<cr>",  desc = "cargo build" },
-                    { "<leader>rb", "<cmd>9Multiterm cargo run<cr>",    desc = "cargo run" },
-                    { "<leader>rs", "<cmd>9Multiterm cargo test<cr>",   desc = "cargo test" },
-                    { "<leader>rm", "<cmd>9Multiterm cargo update<cr>", desc = "cargo update" },
-                },
-                {
-                    buffer = bufnr,
-                })
-        end
-    end,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client.name ~= "rust_analyzer" then
+			return
+		end
+
+		local bufnr = args.buf
+		require("which-key").add({
+			{ "<leader>l",  group = "LSP Actions" },
+			{ "<leader>le", "<cmd>RustExpand<cr>",              desc = "Expand" },
+			{ "<leader>lr", "<cmd>LspRestart<cr>",              desc = "Restart Server" },
+			{ "<leader>r",  group = "Run Configurations" },
+			{ "<leader>rd", "<cmd>9Multiterm cargo build<cr>",  desc = "cargo build" },
+			{ "<leader>rb", "<cmd>9Multiterm cargo run<cr>",    desc = "cargo run" },
+			{ "<leader>rs", "<cmd>9Multiterm cargo test<cr>",   desc = "cargo test" },
+			{ "<leader>rm", "<cmd>9Multiterm cargo update<cr>", desc = "cargo update" },
+		}, { buffer = bufnr })
+	end,
 })
+
+
 
 -- vim.api.nvim_create_autocmd("LspAttach", {
 -- 	callback = function(args)
