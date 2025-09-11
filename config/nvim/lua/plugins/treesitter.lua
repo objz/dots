ce1b6630afbef8f6c8bf733ab775cc88f69861fd
objz <me@objz.dev>
2025-09-11 11:37:30 +0200

    fixed nvim path

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     branch = "master",
-    build = "TSUpdate",
+    build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
